ab140fddb1165a982de407b06b1d4f55ff0f7e6e
Ethan Reno <epreno@live.ca>
2021-02-24 12:23:12 -0500

    Add minor fixes

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models').Book;
 
-/* GET books listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+      try {
+          await cb(req, res, next)
+      } catch (error) {
+          next(error);
+      }
+  }
+}
+
+/* GET home page. */
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  res.render('index', {title: 'Books', books });
+}));
 
 module.exports = router;
